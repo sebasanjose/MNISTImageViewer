419b410af4e8ed5994555475bfda9d30990236ca
sebasanjose <sebaenusa@gmail.com>
2024-10-28 21:27:59 -0700

    Initial Commit

---
+++ MNISTImageViewer/MNISTImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MNISTImageViewerApp.swift
+//  MNISTImageViewer
+//
+//  Created by Sebastian Juarez on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MNISTImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MNISTImageViewerTests/MNISTImageViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MNISTImageViewerTests.swift
+//  MNISTImageViewerTests
+//
+//  Created by Sebastian Juarez on 10/28/24.
+//
+
+import Testing
+@testable import MNISTImageViewer
+
+struct MNISTImageViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
